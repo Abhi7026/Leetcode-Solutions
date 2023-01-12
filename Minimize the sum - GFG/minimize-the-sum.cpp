23e3fb581b99f3e8471b485df394c7de2a7bf14d
Abhinandan Kumar <77017521+Abhi7026@users.noreply.github.com>
2023-01-12 11:18:35 +0530

    Added solution - LeetHub

---
+++ Minimize the sum - GFG/minimize-the-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int minimizeSum(int n, vector<int> arr) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.end());
+        int sum=0;
+        int ans=0;
+        while(pq.size()>1)
+        {
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            sum=(a+b);
+            ans+=sum;
+            pq.push(sum);
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout << obj.minimizeSum(n, arr) << "\n";
+    }
+}
+// } Driver Code Ends
