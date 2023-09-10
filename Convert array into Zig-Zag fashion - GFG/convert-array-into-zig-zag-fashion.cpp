fc82a4ce619e2388b74951b8f51d251d3d5e1550
Abhinandan Kumar <77017521+Abhi7026@users.noreply.github.com>
2023-09-10 23:33:32 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Program for zig-zag conversion of array
+    void zigZag(int a[], int n) {
+        // code here
+         int i=0;          
+        while(i<n){
+            if(a[i]>a[i+1]){
+                int temp=a[i];    
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+            i++;
+            
+            if( i<n-1&&a[i]<a[i+1]){
+                int temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+                
+            }
+            i++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+bool isZigzag(int arr[], int n){
+    int f=1;
+    
+    for(int i=1; i<n; i++){
+        if(f){
+            if(arr[i-1]>arr[i]) return 0;
+        }
+        else{
+            if(arr[i-1]<arr[i]) return 0;
+        }
+        f=f^1;
+    }
+    
+    return 1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        bool check=1;
+        check=isZigzag(arr,n);
+        
+        if(check) cout<<"1\n";
+        else cout<<"0\n";
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
