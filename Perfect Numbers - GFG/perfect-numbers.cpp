c4bfa2270aeca2418273907ea84849facd498203
Abhinandan Kumar <77017521+Abhi7026@users.noreply.github.com>
2023-09-12 09:24:18 +0530

    Added solution - LeetHub

---
+++ Perfect Numbers - GFG/perfect-numbers.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int isPerfectNumber(long long n) {
+        // code here
+      if(n==1)return 0 ;
+        long long sum=0 ;
+        for(int i=2;i<sqrt(n);i++){
+            if(n%i==0){
+                sum+= i;
+                sum+= (n/i) ;
+            }
+        }
+        sum+=1 ;
+        if(sum==n)
+        return 1 ;
+        return 0 ;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
